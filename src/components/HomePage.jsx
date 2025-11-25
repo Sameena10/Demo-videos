5a6b654dbaa21ebf6606a5db76718d207d6b41d3
Sameena10 <89160682+Sameena10@users.noreply.github.com>
2025-11-25 13:23:47 +0530

    check

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,43 +10,30 @@ const HomePage = () => {
     navigate("/login");
   };
 
-  const users = [
-    "Beginner Analysts",
-    "Advanced OSINT Specialists",
-    "Cyber Investigators",
-  ];
-
-  const features = [
-    "Live social media intelligence",
-    "Initial background checks on subjects",
-    "Advanced network link mapping",
-    "Securely archive investigative evidence",
-    "Centralized case management",
-    "Audit-ready operations tracking",
-    "Instant access to multiple data sources",
-    "Tailored technical support",
-  ];
-
   return (
-    <div className="home-container">
-      <div className="hero-section">
-        <h1 className="hero-title">
-          From <span className="highlight">Clue</span> to <span className="highlight">Closure</span> – Fast.
-        </h1>
-        <p className="hero-subtitle">
-          An all-in-one OSINT platform that empowers investigators to uncover critical intelligence.
+    <>
+      <div className="home-container">
+        <div className="hero-section">
+          <h1 className="hero-title">
+            From <span className="highlight">Clue</span> to{" "}
+            <span className="highlight">Closure</span> – Fast.
+          </h1>
 
-        </p>
-        <button className="demo-btn" onClick={handleDemoClick}>
-          Login
-        </button>
-        <p className="hero-subtitle">
-        Log in to master the system and learn to derive intelligence effectively.
-          
-        </p>
+          <p className="hero-subtitle">
+            An all-in-one OSINT platform that empowers investigators to uncover critical intelligence.
+          </p>
+
+          <button className="demo-btn" onClick={handleDemoClick}>
+            Login
+          </button>
+
+          <p className="hero-subtitle">
+            Log in to master the system and learn to derive intelligence effectively.
+          </p>
+        </div>
       </div>
-    </div>
-      //  <Footer />
+        <Footer />
+    </>
   );
 };
 
